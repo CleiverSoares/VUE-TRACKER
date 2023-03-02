3e8c3a79b697ef57c04403a5486e6694ce81624f
CleiverSoares <110869785+CleiverSoares@users.noreply.github.com>
2023-03-02 08:42:09 -0300

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { store, key } from './store/index';
+import { createApp } from "vue";
+import App from "./App.vue";
+import "@fortawesome/fontawesome-free/css/all.css";
+import roteador from "./router";
+
+createApp(App)
+.use(roteador)
+.use(store,key)
+.mount("#app");
